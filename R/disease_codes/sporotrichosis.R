a439c456f90416c215cc63cd78d8c7360f2b1ff6
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-08-27 00:53:37 -0500

    add sporotrichosis

---
+++ R/disease_codes/sporotrichosis.R
@@ -0,0 +1,24 @@
+## Codes for sporotrichosis 
+
+    
+# Description:
+    
+desc <- "Sporotrichosis"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c("1171")))
+    
+icd10_codes <- as.character(children_safe(c("B42")))
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
