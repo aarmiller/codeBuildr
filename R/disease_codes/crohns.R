da1158cd3e6e63667fca9ffb6db2cc19c201cd37
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-10-11 23:14:46 -0500

    Add crohns and UC

---
+++ R/disease_codes/crohns.R
@@ -0,0 +1,24 @@
+## Codes for crohns 
+
+    
+# Description:
+    
+desc <- "Crohn's disease"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c("555")))
+    
+icd10_codes <- as.character(children_safe(c("K50")))
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
---
+++ R/disease_codes/ulcerative_colitis.R
@@ -0,0 +1,24 @@
+## Codes for ulcerative_colitis 
+
+    
+# Description:
+    
+desc <- "Ulcerative colitis"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c("556")))
+    
+icd10_codes <- as.character(children_safe(c("K51")))
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
