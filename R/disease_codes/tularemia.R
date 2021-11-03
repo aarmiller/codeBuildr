b0563881997d7bc0a20e5962dcff8f231a6907a1
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-11-03 15:08:00 -0500

    add tularemia

---
+++ R/disease_codes/tularemia.R
@@ -0,0 +1,24 @@
+## Codes for tularemia 
+
+    
+# Description:
+    
+desc <- "Tularemia"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c("21")))
+    
+icd10_codes <- as.character(children_safe(c("A21")))
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
