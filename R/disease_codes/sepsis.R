b906c5d2384d5b3015b67199a40d6d63df8627bb
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-12-02 18:05:47 -0600

    add sepsis

--- R/disease_codes/sepsis.R
+++ R/disease_codes/sepsis.R
@@ -10,7 +10,10 @@ desc <- "Sepsis"
 icd9_codes <- as.character(children_safe(c("99592","78552","0223","0362","0545",
                                            "449","7907","99591","038","6700")))
 
-icd10_codes <- as.character(children_safe(c("A40","A41")))
+icd10_codes <- as.character(children_safe(c("A039","A021","A207","A217","A227","A239",
+                                            "A241","A267","A280","A282","A327",
+                                            "A392","A393","A394","A40","A41","A158","A1480","A1488",
+                                            "A427","B007","B377","P36")))
 
 # Procedure codes:
 
---
+++ R/disease_codes/sepsis_basic.R
@@ -0,0 +1,25 @@
+## Codes for sepsis_basic 
+
+    
+# Description:
+    
+desc <- "Sepsis Basic Codes"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c("0031","0362","038","99591","99592","78552")))
+    
+icd10_codes <- as.character(children_safe(c("A021","A394","A40","A41","R652")))
+
+
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
