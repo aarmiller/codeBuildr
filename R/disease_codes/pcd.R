9ff561083c10958235a7575507b8d56cb6b8e8d0
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-12-08 00:49:11 -0600

    add pcd

---
+++ R/disease_codes/pcd.R
@@ -0,0 +1,24 @@
+## Codes for pcd 
+
+    
+# Description:
+    
+desc <- "Primary Ciliary Dyskinesia"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c("7488")))
+    
+icd10_codes <- as.character(children_safe(c("Q348")))
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
