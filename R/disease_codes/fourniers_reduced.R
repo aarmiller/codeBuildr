a521436f5de7baf142c6e20cd22c77829d49f2e6
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-12-09 12:01:15 -0600

    add fourniers_reduced

---
+++ R/disease_codes/fourniers_reduced.R
@@ -0,0 +1,24 @@
+## Codes for fourniers_reduced 
+
+    
+# Description:
+    
+desc <- "Fournier's Final Codes (Updated 12/9/21)"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c("60883")))
+    
+icd10_codes <- as.character(children_safe(c("N493")))
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
