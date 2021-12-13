a1a70566676396d05cce892c1b6e6b96286dd892
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-12-13 15:55:27 -0600

    update cf drugs

--- R/drug_codes/cf_drugs.R
+++ R/drug_codes/cf_drugs.R
@@ -3,7 +3,25 @@
 
 trikafta <- c("51167033101")
 
-cf_drug_list <- list(trikafta = trikafta)
+symdeko <- c("51167011301","51167066101")
+
+kalydeco <- c("51167020001","51167020002","51167030001","51167040001",
+              "51167060001")
+
+orkambi <- c("51167050002","51167070002","51167080901","51167090001")
+
+# library(codeBuildr)
+
+# cf_drug_codes <- codeBuildr::get_ndc_from_terms("ivacaftor")
+# 
+# cf_drug_codes %>% 
+#   inner_join(rename(redbook, ndcnum=NDCNUM)) %>% 
+#   select(ndcnum,GENNME,PRODNME)
+
+cf_drug_list <- list(trikafta = trikafta,
+                     symdeko = symdeko,
+                     kalydeco = kalydeco,
+                     orkambi = orkambi)
 
 cf_drug_data <- enframe(cf_drug_list, name = "group", value = "ndc_code") %>% 
   unnest(ndc_code)
