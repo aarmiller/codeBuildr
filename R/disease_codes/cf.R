35e5f561d5218c21b80224e48f50d1da9ca18549
aarmiller <34134737+aarmiller@users.noreply.github.com>
2021-12-27 13:16:16 -0600

    Add smoking codes

--- R/disease_codes/cf.R
+++ R/disease_codes/cf.R
@@ -9,8 +9,10 @@ desc <- "Cystic Fibrosis"
 
 icd9_codes <- as.character(children(c("2770")))
 
-icd10_codes <- as.character(children(c("E84","E840","E841","E8411","E8419",
-                                       "E848","E849")))
+icd10_codes <- as.character(children(as.icd10(c("E84","E840","E841","E8411","E8419",
+                                       "E848","E849")))) %>% unique()
+
+?children
 
 # Procedure codes:
 
---
+++ R/symptom_codes/smoking.R
@@ -0,0 +1,25 @@
+## Codes for smoking 
+
+    
+# Description:
+    
+desc <- "Smoking or Tobacco Use"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c("3051","V1582")))
+    
+icd10_codes <- as.character(children_safe(c("Z720","Z87891","F17","O9933",
+                                            "P042","P9681","Z5731","Z7722","T652")))
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
