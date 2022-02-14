2240de8d1b764dd4465787d9877339e9a08897d7
aarmiller <34134737+aarmiller@users.noreply.github.com>
2022-02-14 09:54:00 -0600

    add ipd codes

---
+++ R/disease_codes/ipd.R
@@ -0,0 +1,24 @@
+## Codes for ipd 
+
+    
+# Description:
+    
+desc <- "Interstitial Pulmonary Diseases"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c("5163")))
+    
+icd10_codes <- as.character(children_safe(c("J84")))
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
