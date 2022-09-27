f9c12715e5ec14c7d41c114d38b713aac4285252
aarmiller <34134737+aarmiller@users.noreply.github.com>
2022-09-27 09:32:01 -0500

    update antibiotics

--- R/drug_codes/antibiotics_all.R
+++ R/drug_codes/antibiotics_all.R
@@ -80,74 +80,25 @@ new_abx <- rbind(abx_old,
 
 write_csv(new_abx,"inst/extdata/rx_all_abx.csv")
 
-# distinct(abx_old,ndc_code) # 27,224
-# distinct(new_abx,ndc_code) # 30,659
 
-# codeBuildr::avail_rx_codes()
-# codeBuildr::load_rx_codes("all_abx") %>%
-#   unlist(use.names = F) %>%
-#   unique() %>%
-#   length()
+## expanded 9/27/22
 
-#
-# write_csv(antibiotic_drug_codes,"data/all_abx_codes.csv")
-#
-# antibiotic_drug_codes %>%
-#   distinct(drug_name,ndcnum) %>%
-#   rename(NDCNUM=ndcnum) %>%
-#   inner_join(redbook) %>%
-#   #filter(ROADS %in% c("Oral",NA,"Route Not Applicable","Multiple Routes")) %>%
-#   filter(ROADS %in% c("Oral")) %>%
-#   select(ndcnum=NDCNUM,drug_name) %>%
-#   write_csv("data/oral_abx_codes.csv")
-#
-#
-# # antibiotic codes for Alan 07/31/20
-#
-#
-#
-# pertusis_abx <- c("azithromycin","clarithromycin","erythromycin")
-#
-# pertusis_drug_codes <- tibble(drug_name=pertusis_abx) %>%
-#   mutate(ndc=map(drug_name,get_ndc_code)) %>%
-#   unnest()
-#
-# pertusis_drug_codes %>%
-#   #distinct(NDCNUM) %>%
-#   inner_join(redbook) %>%
-#   #filter(ROADS %in% c("Oral",NA,"Route Not Applicable","Multiple Routes")) %>%
-#   filter(ROADS %in% c("Oral")) %>%
-#   select(ndcnum=NDCNUM,drug_name) %>%
-#   write_csv("~/OneDrive - University of Iowa/R_projects/Misdiagnosis/pertussis/data/pertusis_abx_codes.csv")
-#
-#
-#
-# get_ndc_code_2 <- function(term){
-#
-#   #redbook <- read_csv("/Volumes/aarmille/Data/redbook.csv")
-#
-#   term <- tolower(term)
-#
-#   ndc_out <- bind_rows(redbook %>%
-#                          filter(str_detect(tolower(GENNME),term)) %>%
-#                          select(NDCNUM),
-#                        redbook %>%
-#                          filter(str_detect(tolower(PRODNME),term)) %>%
-#                          select(NDCNUM),
-#                        redbook %>%
-#                          filter(str_detect(tolower(THRCLDS),term)) %>%
-#                          select(NDCNUM)) %>%
-#     distinct()
-#
-#   return(ndc_out)
-# }
-#
-# get_ndc_code_2("azithromycin")
-#
-# antibiotic_cdi_risk <- antibiotic_drug_codes %>%
-#   distinct(ndcnum) %>%
-#   left_join(tibble(drug_name=c(clindamycin,fluoroquinolones,cephalosporins,monobactams,carbapenems)) %>%
-#               left_join(antibiotic_drug_codes) %>%
-#               distinct(ndcnum) %>%
-#               mutate(high_risk_anti=1L)) %>%
-#   mutate(high_risk_anti=ifelse(is.na(high_risk_anti),0L,high_risk_anti))
+abx_old <- read_csv("inst/extdata/rx_all_abx.csv")
+
+load("/Volumes/AML/truven_mind_projects/antibiotic_risk_categories/antibiotics_groupings_new.RData")
+
+abx_add <- antibiotic_ndc_groups_new %>% 
+  select(name,ndc_code = ndcnum)
+
+abx_add <- abx_add %>% 
+  anti_join(abx_old) %>% 
+  distinct(ndc_code) %>% 
+  mutate(group="uncategorized")
+
+
+new_abx <- bind_rows(abx_old,
+                     abx_add)
+
+write_csv(new_abx,"inst/extdata/rx_all_abx.csv")
+
+rm(list = ls())
---
+++ R/project_sets/cftr_set.R
@@ -0,0 +1,7 @@
+
+
+# Codes for CFTR projects
+
+avail_disease_codes(F)
+
+load_ccs_codes(c(""))
