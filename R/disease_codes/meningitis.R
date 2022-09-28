14e9f100e2d148cf65392626b35a6bf12f00d6fa
aarmiller <34134737+aarmiller@users.noreply.github.com>
2022-09-28 17:02:20 -0500

    fix meningitis codes

--- R/disease_codes/meningitis.R
+++ R/disease_codes/meningitis.R
@@ -7,9 +7,16 @@ desc <- "Meningitis"
 
 # Diagnosis codes:
 
-icd9_codes <- as.character(children_safe(c("00321","0270","0360","0361","320")))
+icd9_codes <- c(as.character(children_safe(c("00321","0270","0360","0361","320"))),
+                icd::icd9cm_hierarchy %>% 
+                  filter(str_detect(long_desc,"meningitis")) %>% 
+                  .$code) %>% unique()
 
-icd10_codes <- as.character(children_safe(c("G00","G03","A87")))
+
+icd10_codes <- c(as.character(children_safe(c("G00","G03","A87"))),
+                 codeBuildr::icd10cm_labels %>% 
+                   filter(str_detect(long_desc,"meningitis")) %>% 
+                   .$code) %>% unique()
 
 # Procedure codes:
 
