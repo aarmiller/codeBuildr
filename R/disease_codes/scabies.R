7f2f2635db778eca276e8317507f6428e2c96100
aarmiller <34134737+aarmiller@users.noreply.github.com>
2022-10-10 16:43:24 -0500

    add scabies

---
+++ R/disease_codes/scabies.R
@@ -0,0 +1,24 @@
+## Codes for scabies 
+
+    
+# Description:
+    
+desc <- "Scabies"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c("1330")))
+    
+icd10_codes <- as.character(children_safe(c("B86")))
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
