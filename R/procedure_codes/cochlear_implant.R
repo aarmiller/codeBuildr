b120ae125992009fdcf322a74f77963438e85956
aarmiller <34134737+aarmiller@users.noreply.github.com>
2023-02-28 16:27:44 -0600

    add cochlear implant

---
+++ R/procedure_codes/cochlear_implant.R
@@ -0,0 +1,24 @@
+## Codes for cochlear_implant 
+
+    
+# Description:
+    
+desc <- "Cochlear Implant"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c()))
+    
+icd10_codes <- as.character(children_safe(c()))
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c("2096","2097","2098")
+    
+pr_codes_icd10 <- c("Z9621","Z45321")
+    
+pr_codes_cpt <- c("69930","0259","92601")
+    
+# Medication codes
+    
+rx_codes <- c()
