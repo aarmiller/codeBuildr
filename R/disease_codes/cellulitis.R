5981bc9aef32dd08f410478809a06159ad49b71f
aarmiller <34134737+aarmiller@users.noreply.github.com>
2023-03-06 16:26:08 -0600

    add cellulits

---
+++ R/disease_codes/cellulitis.R
@@ -0,0 +1,24 @@
+## Codes for cellulitis 
+
+    
+# Description:
+    
+desc <- "Cellulitis"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c("681","682")))
+    
+icd10_codes <- as.character(children_safe(c("L03")))
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
