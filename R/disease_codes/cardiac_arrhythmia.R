7c326ad852ab9b03fa197dc48005c3335ed9d030
aarmiller <34134737+aarmiller@users.noreply.github.com>
2023-03-18 12:13:47 -0500

    add diseases

---
+++ R/disease_codes/cardiac_arrhythmia.R
@@ -0,0 +1,24 @@
+## Codes for cardiac_arrhythmia 
+
+    
+# Description:
+    
+desc <- "Cardiac arrhythmia"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(codeBuildr::get_icd_from_ccs(106)))
+
+icd10_codes <- as.character(children_safe(codeBuildr::get_icd_from_ccs(106,10)))
+
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
---
+++ R/disease_codes/ra.R
@@ -0,0 +1,24 @@
+## Codes for ra 
+
+    
+# Description:
+    
+desc <- "Rheumatoid arthritis"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(codeBuildr::get_icd_from_ccs(202)))
+    
+icd10_codes <- as.character(children_safe(codeBuildr::get_icd_from_ccs(202,10)))
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
