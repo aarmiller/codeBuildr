f37fb39e10d41d39332f764e20714d75fe0a4eb8
aarmiller <34134737+aarmiller@users.noreply.github.com>
2023-03-18 16:53:35 -0500

    add head neck cancer

---
+++ R/disease_codes/head_neck_cancer.R
@@ -0,0 +1,24 @@
+## Codes for head_neck_cancer 
+
+    
+# Description:
+    
+desc <- "Head and Neck Cancer"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(codeBuildr::get_icd_from_ccs(11)))
+
+icd10_codes <- as.character(children_safe(codeBuildr::get_icd_from_ccs(11,icd_version=10)))
+
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
