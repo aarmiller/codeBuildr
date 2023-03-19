5f1513cb48cfd6b10e2d3e11deed13d7805ddf75
aarmiller <34134737+aarmiller@users.noreply.github.com>
2023-03-19 14:42:56 -0500

    add ms

---
+++ R/disease_codes/multiple_myeloma.R
@@ -0,0 +1,25 @@
+## Codes for multiple_myeloma 
+
+    
+# Description:
+    
+desc <- "Multiple myeloma"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(codeBuildr::get_icd_from_ccs(40)))
+
+icd10_codes <- as.character(children_safe(codeBuildr::get_icd_from_ccs(40,icd_version=10)))
+
+    
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
---
+++ R/disease_codes/multiple_sclerosis.R
@@ -0,0 +1,24 @@
+## Codes for multiple_sclerosis 
+
+    
+# Description:
+    
+desc <- "Multiple sclerosis"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(codeBuildr::get_icd_from_ccs(80)))
+
+icd10_codes <- as.character(children_safe(codeBuildr::get_icd_from_ccs(80,icd_version=10)))
+
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
