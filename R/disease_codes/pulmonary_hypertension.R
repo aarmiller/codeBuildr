228ed2c997ba4c7f186908cac48e13925f912080
aarmiller <34134737+aarmiller@users.noreply.github.com>
2023-03-20 10:29:42 -0500

    add pulmonary hypertension

---
+++ R/disease_codes/pulmonary_hypertension.R
@@ -0,0 +1,25 @@
+## Codes for pulmonary_hypertension 
+
+    
+# Description:
+    
+desc <- "Pulmonary Hypertension"
+    
+# Diagnosis codes:
+    
+icd9_codes <- as.character(children_safe(c("4168")))
+    
+icd10_codes <- as.character(children_safe(c("I272")))
+
+
+# Procedure codes:
+    
+pr_codes_icd9 <- c()
+    
+pr_codes_icd10 <- c()
+    
+pr_codes_cpt <- c()
+    
+# Medication codes
+    
+rx_codes <- c()
